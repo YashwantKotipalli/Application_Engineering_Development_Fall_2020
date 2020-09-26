b9637423dafd777fec9fcca2e1d5a77eb85ee556
YashwantKotipalli <kotipalli.ve@northeastern.edu>
2020-09-26 17:01:28 -0400

    Commiting LAB 2

---
+++ LAB_2/src/Business/VitalSignHistory.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author DELL
+ */
+public class VitalSignHistory {
+    
+    public VitalSignHistory(){
+        vitalSignHistory = new ArrayList<VitalSigns>();
+    }
+    
+    private ArrayList<VitalSigns> vitalSignHistory;
+
+    public ArrayList<VitalSigns> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+
+    public void setVitalSignHistory(ArrayList<VitalSigns> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+    
+    
+    public VitalSigns addVitals(){
+        
+        VitalSigns vs = new VitalSigns();
+        vitalSignHistory.add(vs);
+        return vs;
+    }
+    
+    public void deletedVitals(VitalSigns V){
+        
+        vitalSignHistory.remove(V);
+        
+        
+    }
+    
+    
+    
+}
