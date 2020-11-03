4cbcf99e955c66826a1fe73f478a3208b73fb4af
YashwantKotipalli <kotipalli.ve@northeastern.edu>
2020-11-03 16:39:03 -0500

    adding assignment 5

---
+++ Assignment_4/src/Business/Schedule.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Vivek
+ */
+public class Schedule {
+    
+     private ArrayList<FleetSchedule> listOfFlightSchedules;
+      public Schedule()
+    {
+        listOfFlightSchedules = new ArrayList<FleetSchedule>();
+    }
+
+    public ArrayList<FleetSchedule> getListOfFlightSchedules() {
+        return listOfFlightSchedules;
+    }
+
+    public void setListOfFlightSchedules(ArrayList<FleetSchedule> listOfFlightSchedules) {
+        this.listOfFlightSchedules = listOfFlightSchedules;
+    }
+    
+    public FleetSchedule addFleetSchedule(Airline a)
+    {
+        FleetSchedule flightSchedule=new FleetSchedule();
+        flightSchedule.setAirline(a);
+        listOfFlightSchedules.add(flightSchedule);
+        return flightSchedule;
+    }
+}
